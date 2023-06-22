bc076daa945c09a940861ae289d9b2f6c0c03bc0
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-06-22 10:07:40 +0200

    UF-5463: Cleanup

--- src/main/java/se/sundsvall/datawarehousereader/integration/stadsbacken/MeasurementDistrictHeatingDayRepository.java
+++ src/main/java/se/sundsvall/datawarehousereader/integration/stadsbacken/MeasurementDistrictHeatingDayRepository.java
@@ -1,31 +1,29 @@
 package se.sundsvall.datawarehousereader.integration.stadsbacken;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import static se.sundsvall.datawarehousereader.integration.stadsbacken.specification.MeasurementDistrictHeatingDaySpecification.withCustomerOrgId;
+import static se.sundsvall.datawarehousereader.integration.stadsbacken.specification.MeasurementDistrictHeatingDaySpecification.withMeasurementTimestamp;
+import static se.sundsvall.datawarehousereader.integration.stadsbacken.specification.MeasurementDistrictHeatingDaySpecification.withfacilityId;
+
+import java.time.LocalDateTime;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import se.sundsvall.datawarehousereader.integration.stadsbacken.model.measurement.MeasurementDistrictHeatingDayEntity;
 import se.sundsvall.datawarehousereader.integration.stadsbacken.model.measurement.MeasurementDistrictHeatingKey;
 
-import java.time.LocalDateTime;
-
-import static se.sundsvall.datawarehousereader.integration.stadsbacken.specification.MeasurementDistrictHeatingDaySpecification.withCustomerOrgId;
-import static se.sundsvall.datawarehousereader.integration.stadsbacken.specification.MeasurementDistrictHeatingDaySpecification.withMeasurementTimestamp;
-import static se.sundsvall.datawarehousereader.integration.stadsbacken.specification.MeasurementDistrictHeatingDaySpecification.withfacilityId;
-
 @Transactional(readOnly = true)
 @CircuitBreaker(name = "measurementDistrictHeatingDayRepository")
-public interface MeasurementDistrictHeatingDayRepository
-	extends PagingAndSortingRepository<MeasurementDistrictHeatingDayEntity, MeasurementDistrictHeatingKey>, JpaSpecificationExecutor<MeasurementDistrictHeatingDayEntity> {
-	
+public interface MeasurementDistrictHeatingDayRepository extends PagingAndSortingRepository<MeasurementDistrictHeatingDayEntity, MeasurementDistrictHeatingKey>, JpaSpecificationExecutor<MeasurementDistrictHeatingDayEntity> {
+
 	default Page<MeasurementDistrictHeatingDayEntity> findAllMatching(String customerOrgNumber, String facilityId, LocalDateTime dateTimeFrom, LocalDateTime dateTimeTo, Pageable pageable) {
 		return this.findAll(
-			withCustomerOrgId(customerOrgNumber).
-				and(withfacilityId(facilityId)).
-				and(withMeasurementTimestamp(dateTimeFrom, dateTimeTo))
-			, pageable);
+			withCustomerOrgId(customerOrgNumber)
+				.and(withfacilityId(facilityId))
+				.and(withMeasurementTimestamp(dateTimeFrom, dateTimeTo)), pageable);
 	}
 }
