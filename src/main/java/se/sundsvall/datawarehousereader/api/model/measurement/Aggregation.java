6e4e6f3005ef99f9898ee10f8182815ee470ce05
AndreSoderlund <andre.soderlund@b3.se>
2026-01-07 10:04:36 +0100

    Added QUARTER aggregation and removed the un-used YEAR aggregation

--- src/main/java/se/sundsvall/datawarehousereader/api/model/measurement/Aggregation.java
+++ src/main/java/se/sundsvall/datawarehousereader/api/model/measurement/Aggregation.java
@@ -4,9 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Data point aggregation granularity", enumAsRef = true)
 public enum Aggregation {
-
+	QUARTER,
 	HOUR,
 	DAY,
-	MONTH,
-	YEAR
+	MONTH
 }
