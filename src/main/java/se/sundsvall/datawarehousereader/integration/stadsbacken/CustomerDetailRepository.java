37873ca607a10c0ea60e36099eec96296a1a4043
Linus Sjölinder <linus.sjolinder@b3.se>
2026-01-13 09:44:17 +0100

    hydran-908-add-recompile-hint: Add recompile hint annotation and tests

--- src/main/java/se/sundsvall/datawarehousereader/integration/stadsbacken/CustomerDetailRepository.java
+++ src/main/java/se/sundsvall/datawarehousereader/integration/stadsbacken/CustomerDetailRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import se.sundsvall.datawarehousereader.integration.stadsbacken.inspector.WithRecompile;
 import se.sundsvall.datawarehousereader.integration.stadsbacken.model.customer.CustomerDetailEntity;
 
 @Transactional(readOnly = true)
@@ -14,8 +15,7 @@ import se.sundsvall.datawarehousereader.integration.stadsbacken.model.customer.C
 public interface CustomerDetailRepository extends JpaRepository<CustomerDetailEntity, Integer> {
 
 	/**
-	 * /**
-	 * Method for returning all customer engagements connected to sent in organization id
+	 * /** Method for returning all customer engagements connected to sent in organization id
 	 *
 	 * @param  dateTimeFrom   from date to filter on
 	 * @param  organizationId organization id to filter on
@@ -26,6 +26,7 @@ public interface CustomerDetailRepository extends JpaRepository<CustomerDetailEn
 	 *                        be sorted descending, otherwise its sorted ascending)
 	 * @return                list of customer details matching sent in parameters
 	 */
+	@WithRecompile
 	@Query(nativeQuery = true, value = "select * from kundinfo.fnCustomerDetailWithPagingAndSort(:fromDate, :organizationId, :uuid, :pageNumber, :pageSize, :sortBy)")
 	List<CustomerDetailEntity> findWithCustomerEngagementOrgIdAndPartyIds(
 		@Param("fromDate") LocalDateTime dateTimeFrom,
@@ -46,6 +47,7 @@ public interface CustomerDetailRepository extends JpaRepository<CustomerDetailEn
 	 *                        be sorted descending, otherwise its sorted ascending)
 	 * @return                list of customer details matching sent in parameters
 	 */
+	@WithRecompile
 	@Query(nativeQuery = true, value = "select * from kundinfo.fnCustomerDetailWithPagingAndSort(:fromDate, :organizationId, null, :pageNumber, :pageSize, :sortBy)")
 	List<CustomerDetailEntity> findWithCustomerEngagementOrgId(
 		@Param("fromDate") LocalDateTime dateTimeFrom,
--- src/main/java/se/sundsvall/datawarehousereader/integration/stadsbacken/inspector/RecompileStatementInspector.java
+++ src/main/java/se/sundsvall/datawarehousereader/integration/stadsbacken/inspector/RecompileStatementInspector.java
@@ -1,14 +1,16 @@
 package se.sundsvall.datawarehousereader.integration.stadsbacken.inspector;
 
 import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RecompileStatementInspector implements StatementInspector {
 
 	private static final String SQL_START = "select";
 	private static final String RECOMPILE_HINT = " option (recompile)"; // Leading space is intentional
 
 	@Override
-	public String inspect(String sql) {
+	public String inspect(final String sql) {
 		if (RecompileContext.isEnabled() && sql.toLowerCase().startsWith(SQL_START)) {
 			return sql + RECOMPILE_HINT;
 		}
